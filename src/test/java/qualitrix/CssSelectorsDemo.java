767974f81d16848f6c78739a885f8b7e4b3614c4
Kumar Ajay <kmrajay237@gmail.com>
2020-12-10 16:45:56 +0530

    forgot to save the file

--- src/test/java/qualitrix/CssSelectorsDemo.java
+++ src/test/java/qualitrix/CssSelectorsDemo.java
@@ -3,8 +3,40 @@ package qualitrix;
 public class CssSelectorsDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+//		file:///home/qualitrix/Desktop/ws/Training/htmlfiles/xpath.html
+//		Tag and ID
+//		tagname#idValue
+//		input#a1
+		
+//		Tag and Class
+//		tagname.classValue
+//		input.c1
+		
+//		Tag and Attribute
+//		tagname[attributeName='attributeValue']
+//		input[id='a1']
+		
+//		Tag and Attribute and ID/CLASS
+//		tagname.className[attributeName='attributeValue']
+//		tagname#id[attributeName='attributeValue']
+//		input#a1[id='a1']
+//		input.c1[class='c1']
+		
+//		Substring Matches
+//		1. Start With(^)
+//		tagname[attributeName^='prefix attribute value']
+//		a[href^='https://www.g']
+		
+		
+		
+//		2. Ends With($)
+//		tagname[attributeName$='suffix attribute value']
+//		a[href$='m.com']
+		
+		
+		
+//		3. Contains(*)
+//		tagname[attributeName*='substring attribute value']
+//		a[href*='goo']
+}
 }
